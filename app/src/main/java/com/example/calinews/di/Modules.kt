d4c752d86a9c98158a6ecb84c71b0b91ea2a1024
Alberto <alberto.caliman@sinesy.it>
2020-07-24 16:20:39 +0200

    remove useless dependency in gradle files

--- app/src/main/java/com/example/calinews/di/Modules.kt
+++ app/src/main/java/com/example/calinews/di/Modules.kt
@@ -1,14 +1,9 @@
 package com.example.calinews.di
 
 
-import com.supercaliman.data.network.NewsApi
+
 import com.example.calinews.presentation.viewmodel.NewsViewModel
-import com.supercaliman.data.ConfigurationNetwork
-import com.supercaliman.data.NewsRepositoryImpl
-import com.supercaliman.domain.Repository
 import com.supercaliman.domain.getNewsTaskUseCase
-import com.supercaliman.network.Network
-import org.koin.android.ext.koin.androidContext
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
