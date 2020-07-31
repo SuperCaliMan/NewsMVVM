b0d7a8c5af1bcaec78e2ea385bd5a23c5f137dbb
Alberto <alberto.caliman@sinesy.it>
2020-07-31 14:21:23 +0200

    add key api in gradle

--- README.md
+++ README.md
@@ -3,6 +3,4 @@ Activity/Fragment --> ViewModel --> UseCase/Entities/Models --> Repository --> A
 -----------| PRESENTATION |----------------| DOMAIN |--------------------| DATA |--------------------
 
 TODO:
-- separare in moduli data,domain and presentation
-- guardare esempio andrea per migliorare il progetto
 - creare un mock delle news per vedere l'alta testabilità della clean architecture
--- domain/src/main/java/com/supercaliman/domain/getNewsTaskUseCase.kt
+++ domain/src/main/java/com/supercaliman/domain/getNewsTaskUseCase.kt
@@ -5,14 +5,14 @@ import com.supercaliman.domain.model.Result
 
 
 /**
- * Rappresenta le operationi che posso eseguire sul mio oggetto newsResult, va fatta una classe per ogni operazione CRUD
+ * @author Alberto Caliman 31/07/2020
  */
 class getNewsTaskUseCase(val newsRepository: Repository){
 
 
 
    suspend fun execute(): Result<List<NewsArticle>> {
-        val news = newsRepository.getNews("google-news","ba2cd6f3c03c40879416611969d88a9f")
+        val news = newsRepository.getNews("google-news",BuildConfig.APIKEY)
         return news
     }
 }
